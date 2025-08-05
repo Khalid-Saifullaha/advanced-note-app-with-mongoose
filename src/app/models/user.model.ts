6074d7d0aafde1bb9d35ff267480e8c089d0c3cb
Your Name <khalidsaifullaha3@gmail.com>
2025-08-05 16:52:30 +0600

    Query Middleware in Mongoose

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -7,6 +7,7 @@ import {
   UserInstanceMethods,
 } from "../interfaces/user.interface";
 import { th } from "zod/v4/locales/index.cjs";
+import { Note } from "./notes.model";
 
 const addressSchema = new Schema<IAddress>(
   {
@@ -78,6 +79,7 @@ const userSchema = new Schema<IUser, UserInstanceMethods, UserInstanceMethods>(
     timestamps: true,
   }
 );
+
 userSchema.method("hashPassword", async function (plainPassword: string) {
   const password = await bcrypt.hash(plainPassword, 10);
   // this.password = password;
@@ -91,13 +93,38 @@ userSchema.static("hashPassword", async function (plainPassword: string) {
   return password;
 });
 
-userSchema.pre("save", async function () {
+//Pre Hooks
+
+// Document middleware
+
+userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   console.log(this);
+  next();
 });
 
-userSchema.post("save", function (doc) {
+// query middleware
+
+userSchema.pre("find", function (next) {
+  console.log("Inside pre find hook");
+  next();
+});
+
+// post hook
+
+// Document middleware
+userSchema.post("save", function (doc, next) {
   console.log("%s has been saved", doc._id);
+  next();
+});
+
+// query middleware
+userSchema.post("findOneAndDelete", async function (doc, next) {
+  if (doc) {
+    console.log(doc);
+    // await Note.deleteMany({ user: doc._id });
+    next();
+  }
 });
 
 export const User = model<IUser, UserInstanceMethods>("Users", userSchema);
